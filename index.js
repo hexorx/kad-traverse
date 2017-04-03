99afc43df4c671a4107e8828861379466328ab9a
Gordon Hall <gordon@gordonwritescode.com>
2017-04-03 14:12:50 -0400

    remove old, new scaffolding

--- index.js
+++ index.js
@@ -4,4 +4,46 @@
 
 'use strict';
 
-module.exports.TransportDecorator = require('./lib/transport-decorator');
+const NonePlugin = require('./lib/none');
+const UpnpPlugin = require('./lib/upnp');
+const NatPmpPlugin = require('./lib/natpmp');
+const StunPlugin = require('./lib/stun');
+const TurnPlugin = require('./lib/turn');
+const DigletPlugin = require('./lib/diglet');
+
+
+module.exports.none = function(options) {
+  return function(node) {
+    return new NonePlugin(node, options);
+  };
+};
+
+module.exports.upnp = function(options) {
+  return function(node) {
+    return new UpnpPlugin(node, options);
+  };
+};
+
+module.exports.natpmp = function(options) {
+  return function(node) {
+    return new NatPmpPlugin(node, options);
+  };
+};
+
+module.exports.stun = function(options) {
+  return function(node) {
+    return new StunPlugin(node, options);
+  };
+};
+
+module.exports.turn = function(options) {
+  return function(node) {
+    return new TurnPlugin(node, options);
+  };
+};
+
+module.exports.diglet = function(options) {
+  return function(node) {
+    return new DigletPlugin(node, options);
+  };
+};
